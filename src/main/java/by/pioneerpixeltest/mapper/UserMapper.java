fb57dda8ea4d8771005635ba2f6d86c8c4830f09
Dzmitry Karachun <karachun@fptrade.net>
2025-05-15 17:27:24 +0300

    add mupstruct

--- src/main/java/by/pioneerpixeltest/mapper/UserMapper.java
+++ src/main/java/by/pioneerpixeltest/mapper/UserMapper.java
@@ -4,26 +4,26 @@ import by.pioneerpixeltest.dao.dto.UserDto;
 import by.pioneerpixeltest.dao.entity.EmailData;
 import by.pioneerpixeltest.dao.entity.PhoneData;
 import by.pioneerpixeltest.dao.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-import java.math.BigDecimal;
+import java.util.List;
 
-public class UserMapper {
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    @Mapping(target = "emails", source = "emailData", qualifiedByName = "emailDataToEmails")
+    @Mapping(target = "phones", source = "phoneData", qualifiedByName = "phoneDataToPhones")
+    @Mapping(target = "balance", source = "account.balance", defaultValue = "0.00")
+    UserDto convertToDto(User user);
 
-    private UserMapper() {
+    @Named("emailDataToEmails")
+    default List<String> emailDataToEmails(List<EmailData> emailData) {
+        return emailData.stream().map(EmailData::getEmail).toList();
     }
 
-    public static UserDto convertToDto(User user) {
-        UserDto dto = new UserDto();
-        dto.setId(user.getId());
-        dto.setName(user.getName());
-        dto.setDateOfBirth(user.getDateOfBirth());
-        dto.setEmails(user.getEmailData().stream()
-                .map(EmailData::getEmail)
-                .toList());
-        dto.setPhones(user.getPhoneData().stream()
-                .map(PhoneData::getPhone)
-                .toList());
-        dto.setBalance(user.getAccount() != null ? user.getAccount().getBalance() : BigDecimal.ZERO);
-        return dto;
+    @Named("phoneDataToPhones")
+    default List<String> phoneDataToPhones(List<PhoneData> phoneData) {
+        return phoneData.stream().map(PhoneData::getPhone).toList();
     }
 }
--- src/test/java/by/pioneerpixeltest/UserServiceTestConfig.java
+++ src/test/java/by/pioneerpixeltest/UserServiceTestConfig.java
@@ -1,9 +1,11 @@
 package by.pioneerpixeltest;
 
+import by.pioneerpixeltest.mapper.UserMapper;
 import by.pioneerpixeltest.repository.UserRepository;
 import by.pioneerpixeltest.service.UserService;
 import by.pioneerpixeltest.service.impl.UserServiceImpl;
 import by.pioneerpixeltest.util.UserCacheUtil;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,12 +16,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class UserServiceTestConfig {
     @Bean
-    public UserService userService(UserRepository userRepository, UserCacheUtil userCacheUtil) {
-        return new UserServiceImpl(userRepository);
+    public UserService userService(UserRepository userRepository, UserCacheUtil userCacheUtil, UserMapper userMapper) {
+        return new UserServiceImpl(userRepository, userMapper);
     }
 
     @Bean
-    public UserCacheUtil userCacheUtil(UserRepository userRepository) {
-        return new UserCacheUtil(userRepository, null);
+    public UserCacheUtil userCacheUtil(UserRepository userRepository, UserMapper userMapper) {
+        return new UserCacheUtil(userRepository, null, userMapper);
+    }
+
+    @Bean
+    public UserMapper userMapper() {
+        return Mappers.getMapper(UserMapper.class);
     }
 }
